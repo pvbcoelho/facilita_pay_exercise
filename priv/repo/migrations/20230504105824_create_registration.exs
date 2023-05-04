05aba3be0d0148bb030d8ebb8666396a48ebd3c1
Pedro Coelho <pedro.coelho@credisis.com.br>
2023-05-04 09:23:37 -0300

    feat: create migrations

---
+++ priv/repo/migrations/20230504105824_create_registration.exs
@@ -0,0 +1,15 @@
+defmodule FacilitaPayExercise.Repo.Migrations.CreateRegistration do
+  use Ecto.Migration
+
+  def change do
+    create table(:registration, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :partner_id, references(:partner, type: :uuid), null: false
+
+      add :name, :string, null: false
+      add :cpf, :string, null: false
+      add :email, :string, null: false
+      add :inserted_at, :utc_datetime, null: false
+    end
+  end
+end
